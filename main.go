8eba22ae5e9abc32019dbe5dcc442b7c3a56d37a
Ivan <ivan@rahmetapp.kz>
2020-01-12 03:13:21 +0600

    add gorill mux

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello!</h1>")
-
+		fmt.Fprint(w, "<h1>Home page</>")
 }
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Typ", "text/html")
+	fmt.Fprint(w, "<h1>Contact page</>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Typ", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry. Page not found</>")
+}
+
+
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	fmt.Println("listening 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
