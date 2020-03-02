0b62be34267a82a44e08c4f67ae4b3259d23e523
Ivan <ivan@rahmetapp.kz>
2020-03-03 00:59:45 +0600

    added navbar

--- views/views.go
+++ views/views.go
@@ -6,6 +6,7 @@ func NewView(layout string, files ...string) *View {
 
 	files = append(files,
 		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml",
 		"views/layouts/footer.gohtml",
 	)
 
